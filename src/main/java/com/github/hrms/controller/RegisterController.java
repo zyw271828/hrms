35a54798c55b600c917b17a825407056d3f9c026
zyw271828 <zyw_0618@outlook.com>
2019-05-24 12:35:34 +0800

    Using scrypt for password storage

--- src/main/java/com/github/hrms/controller/RegisterController.java
+++ src/main/java/com/github/hrms/controller/RegisterController.java
@@ -1,8 +1,10 @@
 package com.github.hrms.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,11 +18,14 @@ public class RegisterController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping(path = "/add")
-    public @ResponseBody String addNewUser(@RequestParam String username, @RequestParam String email) {
+    @RequestMapping(path = "/add", method = RequestMethod.POST)
+    public @ResponseBody String addNewUser(@RequestParam String username, @RequestParam String email,
+            @RequestParam String password) {
         User n = new User();
         n.setUsername(username);
         n.setEmail(email);
+        SCryptPasswordEncoder encoder = new SCryptPasswordEncoder();
+        n.setPassword(encoder.encode(password));
         userRepository.save(n);
         return "saved";
     }
--- src/main/java/com/github/hrms/model/User.java
+++ src/main/java/com/github/hrms/model/User.java
@@ -12,6 +12,7 @@ public class User {
     private Integer id;
     private String username;
     private String email;
+    private String password;
 
     public Integer getId() {
         return id;
@@ -35,5 +36,13 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
